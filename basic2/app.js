b324a373085c4647ff97a0155ceace45c768b808
irvan <irvina22@gmail.com>
2018-08-07 19:35:43 +0700

    basic 2 Controller directive Angularjs

---
+++ basic2/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("app02",[]);
+
+app.controller("Controller1", function () {
+    this.name="Controller Pertama";
+  })
+
+app.controller("Controller2",function(){
+    this.name ="Controller Kedua";
+})
